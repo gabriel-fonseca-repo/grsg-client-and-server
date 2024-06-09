f1e6c1b9b6833f0898231ba0f17b45a64bd0a9f2
ysrod <90419624+ysrod@users.noreply.github.com>
2024-06-09 16:56:13 -0300

    client soap

--- clients/soap/main_soap.py
+++ clients/soap/main_soap.py
@@ -1,3 +1,104 @@
-# Documentação do client de SOAP python -> https://docs.python-zeep.org/en/master/
+import requests
 
-import zeep as zp
+
+class SoapUser():
+    url = "http://localhost:8081/soap/music"
+
+    main_headers = {
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Connection": "keep-alive",
+        "Content-Type": "application/xml",
+        "Host": "localhost:8081",
+        "User-Agent": "python-requests/2.26.0",
+    }
+
+    
+    def list_all_users(self):
+        return requests.request("POST", self.url,         
+            headers=self.main_headers,
+            data="""
+                <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:msc="http://musicstreaming.com">
+                  <soapenv:Body>
+                      <msc:listAllUsers/>
+                  </soapenv:Body>
+              </soapenv:Envelope>
+            """.strip(),
+        ).text
+
+    
+    def list_all_songs(self):
+        return requests.request("POST", self.url,  
+            headers=self.main_headers,
+            data="""
+              <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:msc="http://musicstreaming.com">
+                  <soapenv:Body>
+                      <msc:listAllSongs/>
+                  </soapenv:Body>
+              </soapenv:Envelope>
+            """.strip(),
+        ).text
+
+    
+    def list_all_playlists_by_user(self, user_id: int):
+        return requests.request("POST", self.url,  
+            headers=self.main_headers,
+            data=f"""
+              <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:msc="http://musicstreaming.com">
+                  <soapenv:Body>
+                      <msc:listAllPlaylistsByUser>
+                          <arg0>{user_id}</arg0>
+                      </msc:listAllPlaylistsByUser>
+                  </soapenv:Body>
+              </soapenv:Envelope>
+            """.strip(),
+        ).text
+
+    
+    def list_all_song_data_of_playlist(self, playlist_id: int):
+        return requests.request("POST", self.url,  
+            headers=self.main_headers,
+            data=f"""
+              <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:msc="http://musicstreaming.com">
+                  <soapenv:Body>
+                      <msc:listAllSongDataOfPlaylist>
+                          <arg0>{playlist_id}</arg0>
+                      </msc:listAllSongDataOfPlaylist>
+                  </soapenv:Body>
+              </soapenv:Envelope>
+            """.strip(),
+        ).text
+
+    
+    def list_all_playlists_with_song(self, song_id: int):
+        return requests.request("POST", self.url,  
+            headers=self.main_headers,
+            data=f"""
+              <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:msc="http://musicstreaming.com">
+                  <soapenv:Body>
+                      <msc:listAllPlaylistsWithSong>
+                          <arg0>{song_id}</arg0>
+                      </msc:listAllPlaylistsWithSong>
+                  </soapenv:Body>
+              </soapenv:Envelope>
+            """.strip(),
+        ).text
+
+if __name__ == "__main__":
+    import random as r
+
+    def random_user_id():
+        return r.randint(0, 19)
+
+    def random_playlist_id():
+        return r.randint(0, 29)
+
+    def random_song_id():
+        return r.randint(0, 99)
+
+    client = SoapUser()
+    print(client.list_all_users())
+    print(client.list_all_songs())
+    print(client.list_all_playlists_by_user(random_user_id()))
+    print(client.list_all_song_data_of_playlist(random_playlist_id()))
+    print(client.list_all_playlists_with_song(random_song_id()))
